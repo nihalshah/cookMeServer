75e134b8d802019584077abd9a6ccd5fbf68cbac
Nihal <nsnihalshah@gmail.com>
2015-07-26 15:05:00 -0500

    Added search by ingredient functionality

--- restAPI.py
+++ restAPI.py
@@ -7,14 +7,31 @@ api = Api(app)
 
 firebase = firebase.FirebaseApplication('https://cookme.firebaseio.com/', None)
 
-class HelloWorld(Resource):
+class getByRecipeName(Resource):
     def get(self, recipeName):
     	recipe = firebase.get('/recipes/'+recipeName+'/'+recipeName+'',None)
     	ingredients = recipe["ingredients"][0]
         return recipe
 
+class getByIngredient(Resource):
+    def get(self, ingredient):
+    	recipeTree = firebase.get('/recipes',None)
+    	recipeList = []
+    	# ingredients = recipe["ingredients"][0]
+    	for shallowRoot in recipeTree:
+    		#print type(recipeTree[shallowRoot])
+    		for deepRoot in recipeTree[shallowRoot]:
+    		 	recipe = recipeTree[shallowRoot][deepRoot]
+    		 	ingredientName = recipe["ingredients"][0]["ingredient name"]
 
-api.add_resource(HelloWorld, '/<string:recipeName>')
+    		 	if ingredientName == ingredient:
+    		 		recipeList.append(recipe)
+
+        return recipeList
+
+
+api.add_resource(getByIngredient, '/ingredient/<string:ingredient>')
+api.add_resource(getByRecipeName, '/recipe/<string:recipeName>')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
