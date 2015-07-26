f188805146a7d406e93bac8b79f1831f405fd2d8
Nihal <nsnihalshah@gmail.com>
2015-07-25 20:06:17 -0500

    Added the String Parameter in the Get function

--- restAPI.py
+++ restAPI.py
@@ -8,12 +8,13 @@ api = Api(app)
 firebase = firebase.FirebaseApplication('https://cookme.firebaseio.com/', None)
 
 class HelloWorld(Resource):
-    def get(self):
-    	result = firebase.get('',None)
-        return result
+    def get(self, recipeName):
+    	recipe = firebase.get('/recipes/'+recipeName+'/'+recipeName+'',None)
+    	ingredients = recipe["ingredients"][0]
+        return recipe
 
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(HelloWorld, '/<string:recipeName>')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
