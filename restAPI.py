0e66288f50023af7d6c6fa974e02ccb48f498f15
Nihal <nsnihalshah@gmail.com>
2015-08-10 22:41:04 -0700

    Removing Nihal to default

--- restAPI.py
+++ restAPI.py
@@ -36,12 +36,13 @@ class getDefaultRecipes(Resource):
     def get(self):
         # dt = datetime.now()
         # print dt.microsecond
-        default = ['Donut', 'Burger', 'Pasta','Ho','Waffles']
+        default = ['Donut', 'Burger','Waffles']
         
        	ret = []
         for recipeName in default:
             ret.append(firebase.get('/recipes/'+recipeName+'/'+recipeName+'',None))
-       # print ret
+
+        print len(ret)
         return ret
 
 api.add_resource(getByIngredient, '/ingredient/<string:ingredient>')
