c9419b08ed999aa974547d1ac84caeb5e1852088
Nihal <nsnihalshah@gmail.com>
2015-08-10 22:36:26 -0700

    Removing Nihal to default

--- restAPI.py
+++ restAPI.py
@@ -36,7 +36,7 @@ class getDefaultRecipes(Resource):
     def get(self):
         # dt = datetime.now()
         # print dt.microsecond
-        default = ['Donut', 'Burger', 'Pasta','Sandwich','Waffles']
+        default = ['Donut', 'Burger', 'Pasta','Tomato Juice','Waffles']
         
        	ret = []
         for recipeName in default:
