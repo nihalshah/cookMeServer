493376887fcad4487b00fbb4a0d62b6e60efcb7d
Nihal <nsnihalshah@gmail.com>
2015-08-10 21:53:15 -0700

    Adding Coffee to Default

--- restAPI.py
+++ restAPI.py
@@ -44,7 +44,6 @@ class getDefaultRecipes(Resource):
 
         # dt = datetime.now()
         # print dt.microsecond
-        ret.append()
         return ret
 
 api.add_resource(getByIngredient, '/ingredient/<string:ingredient>')
